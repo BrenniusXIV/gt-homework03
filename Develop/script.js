69bf07e3e2391d87772e0fd56a9f0eceeb064c17
Brent <brentbaughan1@gmail.com>
2021-03-01 18:41:09 -0500

    assigned character source vars and began writing function

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialChars = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+var passOptions;
+
+
+//Password generator function
+
+function generatePassword(passLength, lowerBool, upperBool, specialBool) {
+  var generatedPassword; //Assign function result
+  if (passLength < 8 || passLength > 128) { //limit password length options
+    return "Please choose a password length between 8 and 128 characters."
+  };
+
+  return generatedPassword;
+};
 
 // Write password to the #password input
 function writePassword() {
