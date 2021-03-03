8bf875de25c36c8c68b5b1f5812607e5eb31b6e0
Brent B <brentbaughan1@gmail.com>
2021-03-02 19:10:54 -0500

    added numbers array and numbersBool parameter, changed logic to follow array length

--- Develop/script.js
+++ Develop/script.js
@@ -2,21 +2,22 @@
 var generateBtn = document.querySelector("#generate");
 var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChars = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 var passOptions;
 
 
 //Password generator function
 
-function generatePassword(passLength, lowerBool, upperBool, specialBool) {
+function generatePassword(passLength, lowerBool, upperBool, numbersBool, specialBool) {
   var generatedPassword = ""; //Assign function result
   var generatedLetter;
   if (passLength < 8 || passLength > 128) { //limit password length options
     return "Please choose a password length between 8 and 128 characters."
   };
   for (var i = 0; i < passLength; i++) {
-    if (lowerBool === true && upperBool === false && specialBool === false) {
-      generatedLetter = lowercaseLetters[(Math.floor(Math.random() * 26))];
+    if (lowerBool === true && upperBool === false && numbersBool === false && specialBool === false) {
+      generatedLetter = lowercaseLetters[(Math.floor(Math.random() * lowercaseLetters.length))];
       generatedPassword = generatedPassword.concat(generatedLetter);
     }
   };
