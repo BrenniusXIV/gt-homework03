f0c1772fe2e8f95e60775aa1e55d11c746e9cc37
Brent <brentbaughan1@gmail.com>
2021-03-03 19:20:30 -0500

    added number input box; added number and checkbox JS objects

--- Develop/script.js
+++ Develop/script.js
@@ -4,11 +4,20 @@ var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "
 var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChars = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
-var passOptions;
+
+//DOM number input; use .value method to check input
+var numInput = document.getElementById("num-inp");
+
+//DOM checkboxes. Use .checked method to determine true/false for conditional logic
+var ckbxLower = document.getElementById("ckbx-lower");
+var ckbxUpper = document.getElementById("ckbx-upper");
+var ckbxNum = document.getElementById("ckbx-num");
+var ckbxSpec = document.getElementById("ckbx-spec");
+
+
 
 
 //Password generator function
-
 function generatePassword(passLength, lowerBool, upperBool, numbersBool, specialBool) {
   var generatedPassword = ""; //Assign function result
   var generatedLetter;
